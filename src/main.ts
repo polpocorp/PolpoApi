62f1e8cba8a4c03c197920c05c12524dffeb3cf4
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2026-03-01 18:30:15 -0300

    refactor server

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(morgan('dev')); // Agrega Morgan para loggear peticiones
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(morgan('dev'));
+  app.enableCors(); // Altamente recomendado para evitar bloqueos desde el frontend
+
+  // En Vercel, necesitamos que la app se inicialice
+  // pero el manejo del puerto es interno de la plataforma.
+  await app.init();
 }
+
+// Es crucial que la función se ejecute para que Vercel cree la instancia
 bootstrap();
