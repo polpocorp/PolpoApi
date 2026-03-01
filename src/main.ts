ed64a81702d3b6fd26f1938b6f750bf4adfdc98a
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2026-03-01 18:45:05 -0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan('dev'));
-  app.enableCors(); // Altamente recomendado para evitar bloqueos desde el frontend
-
+  app.enableCors({
+    origin: '*', // En producción podrías poner solo la URL de tu web
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   // En Vercel, necesitamos que la app se inicialice
   // pero el manejo del puerto es interno de la plataforma.
   await app.init();
